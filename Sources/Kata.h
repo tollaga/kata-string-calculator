385d87da9505dc573f206290bdb00e75f87035ea
Jose Gil <jose.gil@tollaga.com>
2020-04-18 09:11:44 +0200

    Initial import

---
+++ Sources/Kata.h
@@ -0,0 +1,13 @@
+// Tollaga © 2020 - Jose Gil <jose.gil@tollaga.com>
+
+#import <Foundation/Foundation.h>
+
+//! Project version number for kata.
+FOUNDATION_EXPORT double kataVersionNumber;
+
+//! Project version string for kata.
+FOUNDATION_EXPORT const unsigned char kataVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <kata/PublicHeader.h>
+
+
---
+++ Tests/KataTests.swift
@@ -0,0 +1,10 @@
+// Tollaga © 2020 - Jose Gil <jose.gil@tollaga.com>
+
+import XCTest
+import Nimble
+
+@testable import Kata
+
+class KataTests: XCTestCase {
+
+}
